0b09dcaaaa58f1d8af57f01323e9ddeee3cc0191
nikiarohK <149157573+nikiarohK@users.noreply.github.com>
2025-04-08 08:13:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 BOT_TOKEN = ''
 ADMIN_ID = ['']
 GOOGLE_SHEETS_CREDENTIALS_FILE = ''  # Путь к файлу с учетными данными
-GOOGLE_SHEET_NAME = 'Orders'         # Название таблицы
-GOOGLE_SHEET_WORKSHEET = 'Лист1'                    # Название листа в таблице
+GOOGLE_SHEET_NAME = ''         # Название таблицы
+GOOGLE_SHEET_WORKSHEET = ''                    # Название листа в таблице
+IMAGE_FOLDER = ''
